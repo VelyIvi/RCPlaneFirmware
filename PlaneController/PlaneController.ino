0796314129f45cfc0df6008f9769e5a8e877145b
Ivi_ <103999115+VelyIvi@users.noreply.github.com>
2023-10-30 00:53:02 +0100

    LaptopSync

---
+++ PlaneController/PlaneController.ino
@@ -0,0 +1,52 @@
+#include <SPI.h>                      //the communication interface with the modem
+#include "RF24.h"                     //the library which helps us to control the radio modem
+
+#define CE_PIN  22
+#define CSN_PIN 21
+// instantiate an object for the nRF24L01 transceiver
+RF24 radio(CE_PIN, CSN_PIN);
+                                      
+uint8_t address[][6] = { "1Node", "2Node" };
+bool radioNumber = 0;
+bool role = true;  // true = TX node, false = RX node
+
+
+int msg[1]; 
+int sendDelay = 50;
+
+
+void setup(void){
+  Serial.begin(115200);
+  while (!Serial) {
+    // some boards need to wait to ensure access to serial over USB
+  }
+  if (!radio.begin()) {
+    Serial.println(F("radio hardware is not responding!!"));
+    while (1) {}  // hold in infinite loop
+  }
+  
+//  radio.begin();                      //it activates the modem.  
+  radio.setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
+
+  radio.openWritingPipe(address[radioNumber]);  // always uses pipe 0
+  radio.openReadingPipe(1, address[!radioNumber]);  // using pipe 1
+
+  if (role) {
+    radio.stopListening();  // put radio in TX mode
+  } else {
+    radio.startListening();  // put radio in RX mode
+  }
+
+
+}
+
+void loop(void){
+  msg[0] =  map(analogRead(34), 0, 4095, 0, 180);
+  bool report = radio.write(msg, 1);
+  if (report) {
+      Serial.println(F("Transmission successful! "));  // payload was delivered
+      delay(sendDelay);
+    } else {
+      Serial.println(F("Transmission failed or timed out"));  // payload was not delivered
+    }
+}
---
+++ PlaneFirmware/PlaneFirmware.ino
@@ -0,0 +1,55 @@
+#include <ESP32Servo.h>
+#include <SPI.h>      //the communication interface with the modem
+#include "RF24.h"     //the library which helps us to control the radio modem
+
+Servo myServo;        //define the servo name
+
+#define CE_PIN  22
+#define CSN_PIN 21
+// instantiate an object for the nRF24L01 transceiver
+RF24 radio(CE_PIN, CSN_PIN);
+
+uint8_t address[][6] = { "1Node", "2Node" };
+bool radioNumber = 1;
+bool role = false;  // true = TX node, false = RX node
+
+
+int msg[1];
+
+void setup(){
+  myServo.attach(17);                //3 is a digital pin to which servo signal connected.
+
+
+  Serial.begin(115200);
+  while (!Serial) {
+    // some boards need to wait to ensure access to serial over USB
+  }
+  if (!radio.begin()) {
+    Serial.println(F("radio hardware is not responding!!"));
+    while (1) {}  // hold in infinite loop
+  }
+
+  Serial.println(F("Radio started"));
+  
+  radio.setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
+
+  radio.openWritingPipe(address[radioNumber]);  // always uses pipe 0
+  radio.openReadingPipe(1, address[!radioNumber]);  // using pipe 1
+
+  if (role) {
+    radio.stopListening();  // put radio in TX mode
+  } else {
+    radio.startListening();  // put radio in RX mode
+  }
+}
+
+void loop(){
+  uint8_t pipe;
+  if (radio.available(&pipe)) {            //checks whether any data have arrived at the address of the modem
+    bool done = false;              //returns a “true” value if we received some data, or “false” if no data.
+    radio.read(&msg, 1);
+    Serial.println(msg[0]);
+    myServo.write(msg[0]);
+    
+  }
+}
